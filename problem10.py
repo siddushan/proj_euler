e7daaaca866de82b06cdaaa23381c696345a03b3
sys5819 <siddushan@gmail.com>
2017-05-22 13:48:28 +0530

    Create problem10.py

---
+++ problem10.py
@@ -0,0 +1,11 @@
+# problem 10
+
+def is_prime(number):
+    return number > 1 and all(number % i for i in islice(count(2), int(sqrt(number)-1)))
+
+prime_sum = 0
+for q in range(2000000):
+    if is_prime(q):
+        prime_sum += q
+
+print prime_sum
