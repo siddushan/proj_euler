ec27db04ce8a11839e2e640928ee3d7629fb77f7
sys5819 <siddushan@gmail.com>
2017-05-22 14:03:14 +0530

    Update problem15.py

--- problem15.py
+++ problem15.py
@@ -1,5 +1,6 @@
 # problem 15
 # literally just grid area choose dimension (so for 20x20, 40C20)
+from math import factorial
 
 def ncr(n, r):
     return factorial(n) / (factorial(r) * factorial(n - r))
